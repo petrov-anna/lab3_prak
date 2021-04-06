e7384f5b61f7ab13f704c5629767bcf17f450121
petrov-anna <anna.petrov-petrov@yandex.ru>
2021-04-06 14:07:23 +0300

    Выполнение дополнительного 3 задания. При разрыве соединения клиентом сервер продолжает слушать данный порт и доступен для повторного подключения

--- client.py
+++ client.py
@@ -10,9 +10,9 @@ while True:
     message = input("Введите сообщение => ")
     sock.send(message.encode())
     print("Отправили сообщение")
-    data = sock.recv(1024)
-    print(f'Получили сообщение => {data.decode()}')
     if message == 'exit':
         print(f'Мы были отключены от сервера')
         sock.close()
         break
+    data = sock.recv(1024)
+    print(f'Получили сообщение => {data.decode()}')
--- server.py
+++ server.py
@@ -1,30 +1,31 @@
 import socket
 
-print("~Запуск сервера~")
-sock = socket.socket()
-sock.bind(('', 9090))
-print("~Начало прослушивания порта~")
-sock.listen(0)
-print("~Подключение клиента~")
-conn, addr = sock.accept()
-print(f"Клиент {addr} был подключен")
-
-msg = ''
-
 while True:
-    data = conn.recv(1024)
-    if not data:
-        print(f"Клиент {addr} был отключен")
-        break
-    print(f'Получили сообщение: {data.decode()}')
-    msg += data.decode()
-    conn.send(data)
-
-    if data.decode() == 'exit':
-        print("Сервер остановили")
+    msg = ''
+    running = True
+    while running:
+        print("~Запуск сервера~")
+        sock = socket.socket()
+        sock.bind(('', 9090))
+        print("~Начало прослушивания порта~")
+        sock.listen(0)
+        print("~Ожидаем подключение клиента~")
+        conn, addr = sock.accept()
+        print(f"Клиент {addr} был подключен")
         running = False
-        conn.close()
-        break
-    print(f'Отправленное сообщение: {data.decode()}')
+    running = True
+    # conn.send(data)
+    while running:
+        data = conn.recv(1024)
+        if not data:
+            print(f"Клиент {addr} был отключен")
+            break
+        if data.decode() == 'exit':
+            print("!!!Сервер остановили!!!")
+            running = False
+            break
+        print(f'Получили сообщение: {data.decode()}')
+        msg += data.decode()
+        conn.send(data)
+        print(f'Отправленное сообщение: {data.decode()}')
 
-conn.close()
