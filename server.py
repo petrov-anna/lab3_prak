a5e38c985b6e8d84245d2c6128055ff21822ac80
petrov-anna <anna.petrov-petrov@yandex.ru>
2021-04-06 14:57:32 +0300

    Выполнение дополнительного 6 задания. Изменить код сервера, он автоматически изменял номер порта, если он уже занят. Сервер выводит в консоль номер порта, который слушает.

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import errno
 import socket
 
 
@@ -12,42 +13,82 @@ def writeData(data):
         file.write(f'{data}\n')
 
 
+def freePort(sock, s_port):
+    try:
+        sock.bind(('', s_port))
+        print(f'Порт: {s_port}')
+        return True
+    except socket.error as e:
+        if e.errno == errno.EADDRINUSE:
+            s_port += 1
+            return freePort(sock, s_port)
+
+
 clean()
+sock = socket.socket()
+s_port = 9090
+# while True:
+#     msg = ''
+#     running = True
+#     while running:
+#         print("~Запуск сервера~")
+#         writeData("~Запуск сервера~")
+#         sock = socket.socket()
+#         sock.bind(('', 9090))
+#         print("~Начало прослушивания порта~")
+#         writeData("~Начало прослушивания порта~")
+#         sock.listen(0)
+#         print("~Ожидаем подключение клиента~")
+#         writeData("~Ожидаем подключение клиента~")
+#         conn, addr = sock.accept()
+#         print(f"Клиент {addr} был подключен")
+#         writeData(f"Клиент {addr} был подключен")
+#         running = False
+#     running = True
+#     # conn.send(data)
+#     while running:
+#         data = conn.recv(1024)
+#         if not data:
+#             print(f"Клиент {addr} был отключен")
+#             writeData(f"Клиент {addr} был отключен")
+#             break
+#         if data.decode() == 'exit':
+#             print("!!!Сервер остановили!!!")
+#             writeData("!!!Сервер остановили!!!")
+#             running = False
+#             break
+#         print(f'Получили сообщение: {data.decode()}')
+#         writeData(f'Получили сообщение: {data.decode()}')
+#         msg += data.decode()
+#         conn.send(data)
+#         print(f'Отправленное сообщение: {data.decode()}')
+#         writeData(f'Отправленное сообщение: {data.decode()}')
 
-while True:
-    msg = ''
-    running = True
-    while running:
-        print("~Запуск сервера~")
-        writeData("~Запуск сервера~")
-        sock = socket.socket()
-        sock.bind(('', 9090))
-        print("~Начало прослушивания порта~")
-        writeData("~Начало прослушивания порта~")
-        sock.listen(0)
-        print("~Ожидаем подключение клиента~")
-        writeData("~Ожидаем подключение клиента~")
+
+if freePort(sock, s_port):
+    print("~Начало прослушивания порта~")
+    writeData("~Начало прослушивания порта~")
+    sock.listen(1)
+
+    while True:
+        file = open('logFile.txt', 'a')
         conn, addr = sock.accept()
-        print(f"Клиент {addr} был подключен")
-        writeData(f"Клиент {addr} был подключен")
-        running = False
-    running = True
-    # conn.send(data)
-    while running:
-        data = conn.recv(1024)
-        if not data:
-            print(f"Клиент {addr} был отключен")
-            writeData(f"Клиент {addr} был отключен")
-            break
-        if data.decode() == 'exit':
-            print("!!!Сервер остановили!!!")
-            writeData("!!!Сервер остановили!!!")
-            running = False
-            break
-        print(f'Получили сообщение: {data.decode()}')
-        writeData(f'Получили сообщение: {data.decode()}')
-        msg += data.decode()
-        conn.send(data)
-        print(f'Отправленное сообщение: {data.decode()}')
-        writeData(f'Отправленное сообщение: {data.decode()}')
-
+        running = True
+        print(f'Клиент {addr} был подключен')
+        writeData(f'Клиент {addr} был подключен\n')
+        while running:
+            data = conn.recv(1024)
+            if not data:
+                print(f'Клиент {addr} был отключен')
+                writeData(f'Клиент {addr} был отключен\n')
+                break
+            if data.decode() == 'exit':
+                print('!!!Сервер остановили!!!')
+                writeData('!!!Сервер остановили!!!\n')
+                running = False
+                break
+            print(f'Получили сообщение:{data.decode()}')
+            print(f'Отправленное сообщение: {data.decode()}')
+            writeData(f'Получили сообщение:{data.decode()}')
+            writeData(f'Отправленное сообщение: {data.decode()}')
+            conn.send(data)
