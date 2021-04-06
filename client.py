e2f23b33024cd7d2604828dc3a9d44c11f8d10c8
petrov-anna <anna.petrov-petrov@yandex.ru>
2021-04-06 14:39:11 +0300

    Выполнение дополнительного 4 задания. Пользователь вводить номер порта и ip-адрес. Реализован безопасный ввод данных и значения по умолчанию.

--- client.py
+++ client.py
@@ -1,10 +1,55 @@
 import socket
 from time import sleep
 
+
+def portVer(port):
+    try:
+        return True if 1024 <= int(port) <= 65535 else False
+    except ValueError:
+        return False
+
+
+def ipVer(ip):
+    try:
+        sum = 0
+        if ip == 'localhost':
+            return True
+        else:
+            parts = ip.split(".")
+            # print(len(parts))
+            if len(parts) == 4:
+                for part in parts:
+                    if 0 <= int(part) <= 255:
+                        sum += 1
+            else:
+                return False
+            if sum != 4:
+                return False
+    except ValueError:
+        return False
+
+
+portInput = input("Введите порт: ")
+ipInput = input("Введите ip: ")
+port_auto = 9090
+ip_auto = "127.0.0.1"
+
+if portVer(portInput) is False:
+    print(f'Вы ввели некорректные данные, порт по умолчанию -  {port_auto}')
+    portInput = int(port_auto)
+portInput = int(portInput)
+
+if ipVer(ipInput) is False:
+    print(f'Вы ввели некорректные данные, ip по умолчанию -  {ip_auto}')
+    ipInput = ip_auto
+ipInput = str(ipInput)
+
+
+print(f'Ip -> {ipInput}, Порт -> {portInput}')
 sock = socket.socket()
 sock.setblocking(1)
 print("~Соединение с сервером~")
-sock.connect(('127.0.0.1', 9090))
+sock.connect((ipInput, portInput))
 
 while True:
     message = input("Введите сообщение => ")
